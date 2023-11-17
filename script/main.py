082b27ed557595b2e43b521cdfdb91534694de57
kikiymini <caramayo@fi.uba.ar>
2023-11-16 22:14:34 -0300

    Agrego principal

---
+++ script/main.py
@@ -0,0 +1,18 @@
+def main():
+    # Datos del problema
+    m = 200  # masa (kg)
+    k = 25000  # constante elástica del muelle (N/m)
+    lambda_ = 0  # constante de amortiguación (Ns/m)
+    c = 0.1  # cota del terreno (m)
+
+    # Condiciones iniciales
+    y0 = 0.1  # posición inicial
+    v0 = 0.0  # velocidad inicial
+
+    # Parámetros de simulación
+    tiempo_total = 5.0  # segundos
+
+    return
+
+if __name__ == "__main__":
+    main()
