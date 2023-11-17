2ade30c4c70540becb651e5b36d0d98865a94bbd
kikiymini <caramayo@fi.uba.ar>
2023-11-16 22:19:26 -0300

    Modifico variables del problema

--- script/main.py
+++ script/main.py
@@ -3,7 +3,13 @@ import rungekutta2
 
 def main():
     # Datos del problema
-    m = 200  # masa (kg)
+    padronJI = 0
+    padronTB = 0
+    padronCZ = 106260
+    denominador = 200
+    m1 = padronJI/denominador  # masa (kg)
+    m2 = padronTB/denominador
+    m3 = padronCZ/denominador
     k = 25000  # constante elástica del muelle (N/m)
     lambda_ = 0  # constante de amortiguación (Ns/m)
     c = 0.1  # cota del terreno (m)
@@ -13,7 +19,7 @@ def main():
     v0 = 0.0  # velocidad inicial
 
     # Parámetros de simulación
-    tiempo_total = 5.0  # segundos
+    delta_tiempo = 0.005  # segundos
 
 
     return
